875b707052b6bf8b93719a07a75dadf2934c8945
andrefliper <andrefilipe3105@gmail.com>
2025-06-17 19:45:55 -0300

    Adição de sobrescrita de método concreto na classe cachorro

--- Cachorro.java
+++ Cachorro.java
@@ -7,4 +7,10 @@ public class Cachorro extends Pet {
     public String falar() {
         return nome + " diz: Au Au!";
     }
+
+    @Override
+    public String passear() {
+        felicidade = Math.min(100, felicidade + 60); // diferente da base: +60 em vez de +50
+        return nome + " ficou felizasso com o passeio!!";
+    }
 }
